adeeffda3be3f433c98fc7290fc9ddb6a7951c1e
lowmingray <lowmr-wm23@student.tarc.edu.my>
2025-04-14 16:05:42 +0800

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.cp2qesi406qm.us-east-1.rds.amazonaws.com"
+customhost = "employee.cz3ryvqbtgxt.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Aacs3253"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "lowmingray-employee"
 customregion = "us-east-1"
 
